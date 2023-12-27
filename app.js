db18add72a8f710d4312ff8947e4054b4a87743e
nvhoangviet <nvhoangviet@yahoo.com>
2023-12-27 07:25:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,59 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const { Web3 } = require("web3");
+const NODE_URL = "wss://mainnet.infura.io/ws/v3/297bd5e1cfef49b1bf73a90071c7b5da";
+const UNISWAP_ROUTER_ADDRESS = "0x7a250d5630b4cf539739df2c5dacb4c659f2488d";
+const SWAP_EXACT_ETH_FOR_TOKEN_SIGNATURE = "0x7ff36ab5";
+const SWAP_EXACT_ETH_FOR_FEE_TOKEN_SIGNATURE = "0xb6f9de95";
 
-app.get("/", (req, res) => res.type('html').send(html));
+const provider = new Web3.providers.WebsocketProvider(
+  "wss://mainnet.infura.io/ws/v3/297bd5e1cfef49b1bf73a90071c7b5da",
+  {
+      clientConfig: {
+          keepalive: true,
+          keepaliveInterval: 60000
+      },
+      reconnect: {
+          auto: true,
+          delay: 5000,
+          maxAttempts: 5,
+          onTimeout: false
+      }
+  }
+);
+provider.on('connect', () => {
+  console.log("Websocket connected.");
+});
+provider.on('close', (event) => {
+  console.log(event);
+  console.log("Websocket closed.");
+});
+provider.on('error', (error) => {
+  console.error(error);
+});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const web3 = new Web3(provider);
+web3.eth.net.isListening().then((response)=>{
+  if (response === true){
+    console.log("Hoang check connect ", response);
+    monitorNewBlock();
+  }
+})
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
+async function monitorNewBlock(){
+  const subscription = await web3.eth.subscribe('newBlockHeaders');
+  subscription.on('data', async (blockHeader)=>{
+    console.log('Get new block number', blockHeader.number);
+    const block = await web3.eth.getBlock(blockHeader.number, true);
+    // console.log("Hoang check block ne ", block);
+    block.transactions.forEach((trx)=>{
+      if (trx.to && trx.to.toLowerCase() === UNISWAP_ROUTER_ADDRESS){
+        if (trx.input && (trx.input.startsWith(SWAP_EXACT_ETH_FOR_TOKEN_SIGNATURE) || trx.input.startsWith(SWAP_EXACT_ETH_FOR_FEE_TOKEN_SIGNATURE))){
+          console.log("Hash: ", trx.hash);
+          console.log("From: ", trx.from);
+          console.log("Value: ", web3.utils.fromWei(trx.value, "ether"), " ETH");
+          console.log("Input: ", trx.input);
+        }
+        // console.log('Hoang check trx', trx);
       }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    });
+  });
+}
